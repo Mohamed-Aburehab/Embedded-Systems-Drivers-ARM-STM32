47c6fd4477c44a577f838200f5dfc6aaa7d01712
Mohamed Aburehab <mohamed.aburehab@gmail.com>
2022-07-17 04:13:07 +0200

    adding comments

--- 01-LIB/02-BIT_MATH/BIT_MATH.h
+++ 01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -9,9 +9,13 @@
 #ifndef BIT_MATH_H_
 #define BIT_MATH_H_
 
+/* Setting the bit to 1. */
 #define SET_BIT(VAR,BIT)        (VAR) |=  (1 << (BIT))
+/* Clearing the bit. */
 #define CLR_BIT(VAR,BIT)        (VAR) &= ~(1 << (BIT))
+/* Getting the value of the bit in the variable VAR at the position BIT. */
 #define GET_BIT(VAR,BIT)        ((VAR) >> (BIT)) & 1
+/* Toggling the bit. */
 #define TOG_BIT(VAR,BIT)        (VAR) ^=  (1 << (BIT))
 
 #endif
