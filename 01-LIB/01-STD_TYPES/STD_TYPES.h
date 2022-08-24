320ae315e60d02cb1c9fb23eb37e0712fdf555ce
Mohamed <mohamed.aburehab@gmail.com>
2022-08-24 17:00:15 +0200

    adding error states and s8

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -10,8 +10,18 @@
 #ifndef STD_TYPES_H_
 #define STD_TYPES_H_
 
+// Standard Types
 typedef unsigned char           u8;
 typedef unsigned short int      u16;
 typedef unsigned long int       u32;
+
+typedef signed char             s8;
+
+// Error States
+#define NO_ERROR 1
+#define ERROR_WRONG_FUNCTION_ARGUMENTS 2
+#define ERROR_NULL_POINTER 3
+#define ERROR_TIMEOUT 4
+#define BUSY_FUNCTION 5 
 
 #endif
