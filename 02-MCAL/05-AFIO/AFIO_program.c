00c0a3184667fd33398bb055cc6a3534c908b010
Mohamed <mohamed.aburehab@gmail.com>
2022-08-28 19:53:53 +0200

    AFIO driver

---
+++ 02-MCAL/05-AFIO/AFIO_program.c
@@ -0,0 +1,24 @@
+/*****************************************************************************/
+/*****************************************************************************/
+/**********************    Author : Mohamed Aburehab    **********************/
+/**********************    Layer  : MCAL                **********************/
+/**********************    SWD    : AFIO                **********************/
+/**********************    Version: 1.0                 **********************/
+/*****************************************************************************/
+/*****************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "AFIO_interface.h"
+#include "AFIO_private.h"
+#include "AFIO_config.h"
+
+void MAFIO_voidSetEXTIConfiguration(u8 Copy_u8Line, u8 Copy_u8PortMap){
+    u8 Local_u8RegIndex = Copy_u8Line/4;
+    Copy_u8Line = Copy_u8Line%4;
+
+    AFIO->EXTICR[Local_u8RegIndex] &= ~((0b1111) << (Copy_u8Line * 4));
+    AFIO->EXTICR[Local_u8RegIndex] |= ((Copy_u8PortMap) << (Copy_u8Line * 4));
+
+}
