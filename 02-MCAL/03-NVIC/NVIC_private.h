fc6719289d68c611bca95e31f401c81ee6396938
Mohamed <mohamed.aburehab@gmail.com>
2022-08-30 08:30:34 +0200

    adding volatile keyword for all registers even read and write ones

--- 02-MCAL/03-NVIC/NVIC_private.h
+++ 02-MCAL/03-NVIC/NVIC_private.h
@@ -12,28 +12,28 @@
 
                             /* Register Definitions */
 /* Enable External Interrupts from 0 to 31 register */
-#define NVIC_ISER0                      *((u32*) 0xE000E100) 
+#define NVIC_ISER0                      *((volatile u32*) 0xE000E100) 
 
 /* Enable External Interrupts from 32 to 63 register */
-#define NVIC_ISER1                      *((u32*) 0xE000E104) 
+#define NVIC_ISER1                      *((volatile u32*) 0xE000E104) 
 
 /* Disable External Interrupts from 0 to 31 register */
-#define NVIC_ICER0                      *((u32*) 0xE000E180) 
+#define NVIC_ICER0                      *((volatile u32*) 0xE000E180) 
 
 /* Disable External Interrupts from 32 to 63 register */
-#define NVIC_ICER1                      *((u32*) 0xE000E184) 
+#define NVIC_ICER1                      *((volatile u32*) 0xE000E184) 
 
 /* Set Pending Flag for Interrupts from 0 to 31 register */
-#define NVIC_ISPR0                      *((u32*) 0xE000E200) 
+#define NVIC_ISPR0                      *((volatile u32*) 0xE000E200) 
 
 /* Set Pending Flag for Interrupts from 32 to 63 register */
-#define NVIC_ISPR1                      *((u32*) 0xE000E204) 
+#define NVIC_ISPR1                      *((volatile u32*) 0xE000E204) 
 
 /* Clear Pending Flag for Interrupts from 0 to 31 register */
-#define NVIC_ICPR0                      *((u32*) 0xE000E280) 
+#define NVIC_ICPR0                      *((volatile u32*) 0xE000E280) 
 
 /* Clear Pending Flag for Interrupts from 32 to 63 register */
-#define NVIC_ICPR1                      *((u32*) 0xE000E284)
+#define NVIC_ICPR1                      *((volatile u32*) 0xE000E284)
 
 /* Active Bit Flag for Interrupts from 0 to 31 register */
 #define NVIC_IABR0                      *((volatile u32*) 0xE000E300) 
