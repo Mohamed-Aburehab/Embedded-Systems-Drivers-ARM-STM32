7ef83829b99ae0571e11cdd03f2ff591820d174b
Mohamed <mohamed.aburehab@gmail.com>
2022-09-02 09:47:04 +0200

    adding parentheses for priority of operations

--- 01-LIB/02-BIT_MATH/BIT_MATH.h
+++ 01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -3,7 +3,7 @@
 /**********************    Author : Mohamed Aburehab    **********************/
 /**********************    Layer  : LIB                 **********************/
 /**********************    SWD    : BIT MATH            **********************/
-/**********************    Version: 1.0                 **********************/
+/**********************    Version: 2.0                 **********************/
 /*****************************************************************************/
 /*****************************************************************************/
 
@@ -11,12 +11,12 @@
 #define BIT_MATH_H_
 
 /* Setting the bit to 1. */
-#define SET_BIT(VAR,BIT)        (VAR) |=  (1 << (BIT))
+#define SET_BIT(VAR,BIT)        ((VAR) |=  (1 << (BIT)))
 /* Clearing the bit. */
-#define CLR_BIT(VAR,BIT)        (VAR) &= ~(1 << (BIT))
+#define CLR_BIT(VAR,BIT)        ((VAR) &= ~(1 << (BIT)))
 /* Getting the value of the bit in the variable VAR at the position BIT. */
-#define GET_BIT(VAR,BIT)        ((VAR) >> (BIT)) & 1
+#define GET_BIT(VAR,BIT)        (((VAR) >> (BIT)) & 1)
 /* Toggling the bit. */
-#define TOG_BIT(VAR,BIT)        (VAR) ^=  (1 << (BIT))
+#define TOG_BIT(VAR,BIT)        ((VAR) ^=  (1 << (BIT)))
 
 #endif
