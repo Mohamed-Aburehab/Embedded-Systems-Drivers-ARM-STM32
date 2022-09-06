809c92f01344e01615e7a135ec965a4f220206fe
Mohamed <mohamed.aburehab@gmail.com>
2022-09-06 15:27:31 +0200

    Adding Function comments & Port Config Macros to AFIO

--- 02-MCAL/05-AFIO/AFIO_interface.h
+++ 02-MCAL/05-AFIO/AFIO_interface.h
@@ -10,6 +10,18 @@
 #ifndef AFIO_INTERFACE_H_
 #define AFIO_INTERFACE_H_
 
+/*AFIO EXTI Port Map Configuration*/
+#define MAFIO_EXTI_PORTA_CONFIG             0
+#define MAFIO_EXTI_PORTB_CONFIG             1
+#define MAFIO_EXTI_PORTC_CONFIG             2
+
+/**
+ * It takes a line number and a port map and sets the port map for that line in the
+ * EXTI configuration register
+ * 
+ * @param Copy_u8Line The line number of the interrupt.
+ * @param Copy_u8PortMap The port you want to map to the EXTI line.
+ */
 void MAFIO_voidSetEXTIConfiguration(u8 Copy_u8Line, u8 Copy_u8PortMap);
 
 #endif
--- 02-MCAL/05-AFIO/AFIO_program.c
+++ 02-MCAL/05-AFIO/AFIO_program.c
@@ -14,6 +14,13 @@
 #include "AFIO_private.h"
 #include "AFIO_config.h"
 
+/**
+ * It takes a line number and a port map and sets the port map for that line in the
+ * EXTI configuration register
+ * 
+ * @param Copy_u8Line The line number of the interrupt.
+ * @param Copy_u8PortMap The port you want to map to the EXTI line.
+ */
 void MAFIO_voidSetEXTIConfiguration(u8 Copy_u8Line, u8 Copy_u8PortMap){
     u8 Local_u8RegIndex = Copy_u8Line/4;
     Copy_u8Line = Copy_u8Line%4;
